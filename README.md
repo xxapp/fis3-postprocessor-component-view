66dbaa3a01a8368e689490de0551f56f6e29c3c7
mengxiangxiang <mengxiangxiang@cdavatar.com>
2017-01-17 11:32:31 +0800

    Initial Code

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# fis3-postprocessor-component-view
-Baidu FIS3 plugin for component view
+## 用法
+``` html
+<!-- 处理之前 -->
+<style>
+    .control-file :global .input-group-btn > input {
+        position: absolute;
+        clip: rect(0 0 0 0);
+        outline: none;
+    }
+</style>
+<template>
+    <div class="form-group control-file">
+        <label class="control-label">头像</label>
+        <div class="input-group">
+            <input type="text" class="form-control" disabled>
+            <span class="input-group-btn">
+                <label class="btn btn-info" for="foo">选择图片</label>
+                <input type="file" name="file" id="foo">
+            </span>
+        </div>
+    </div>
+</template>
+```
+
+``` js
+// fis-conf.js
+// ...
+fis.match('/components/**/*.html', {
+    postprocessor: fis.plugin('component-view', { })
+});
+// ...
+```
+
+``` html
+<!-- 处理之后 -->
+<style>
+    .control-file_wvqyx_3 .input-group-btn > input {
+        position: absolute;
+        clip: rect(0 0 0 0);
+        outline: none;
+    }
+</style>
+<template>
+    <div class="form-group control-file_wvqyx_3 ">
+        <label class="control-label">头像</label>
+        <div class="input-group">
+            <input type="text" class="form-control" disabled>
+            <span class="input-group-btn">
+                <label class="btn btn-info" for="foo">选择图片</label>
+                <input type="file" name="file" id="foo">
+            </span>
+        </div>
+    </div>
+</template>
+```
---
+++ index.js
@@ -0,0 +1,80 @@
+var cssModulesSync = require('postcss-modules-sync');
+var postcss = require('postcss');
+var htmlCompiler = require('vue-template-compiler');
+
+module.exports = function (content, file, setting) {
+    var output = htmlCompiler.parseComponent(content.toString(), { pad: true });
+    var exportedTokens = {}, content;
+    var rAttr = /(class[\s\n]*=[\s\n]*)('|")([^\2]*?)\2/g, result;
+    var rClassName = /[\n\s]*([^\n\s]*)[\n\s]*/g;
+
+    output['styles'].forEach(function(item, index) {
+        if (item.content) {
+            var styleFileName, styleFile, styleContent;
+            var tokenFile, tokenContent;
+
+            if (output['styles'].length == 1) {
+                styleFileName = file.realpathNoExt + '.css';
+            } else {
+                styleFileName = file.realpathNoExt + '-' + index + '.css';
+            }
+
+            var styles = postcss([
+                cssModulesSync.default({
+                    getTokens: function (tokens) {
+                        exportedTokens = fis.util.merge(exportedTokens ,tokens);
+                    }
+                })
+            ]).process(item.content).css;
+
+            // css
+            styleFile = fis.file.wrap(styleFileName);
+
+            styleContent = fis.compile.partial(styles, file, {
+                ext: item.lang || 'css',
+                isCssLike: true
+            });
+
+            styleFile.cache = file.cache;
+            styleFile.isCssLike = true;
+            styleFile.setContent(styleContent);
+            fis.compile.process(styleFile);
+            styleFile.links.forEach(function(derived) {
+                file.addLink(derived);
+            });
+            file.derived.push(styleFile);
+            file.addRequire(styleFile.getId());
+        }
+    });
+
+    // token
+    // tokenFile = fis.file.wrap(file.realpathNoExt + '1.js');
+    // tokenContent = fis.compile.partial('define(\'ms-control-file-token\', function(require, exports, module) {modules.exports = ' + JSON.stringify(exportedTokens) + '});', file, {
+    //     ext: 'js',
+    //     isJsLike: true
+    // });
+
+    // tokenFile.cache = file.cache;
+    // tokenFile.isJsLike = true;
+    // tokenFile.setContent(tokenContent);
+    // file.derived.push(tokenFile);
+    // file.addRequire(tokenFile.getId());
+
+    content = output.template.content || content;
+    do {
+        content = content.replace(rAttr, function (s, attrName, quote, attrValue) {
+            if (s && attrValue) {
+                // 'class=' + '"' + 'btn btn-info' + '"'
+                return attrName + quote + attrValue.replace(rClassName, function (s, className, index) {
+                    console.log((index ? ' ' : '') + (exportedTokens[className] || className));
+                    return (index ? ' ' : '') + (exportedTokens[className] || className);
+                }) + quote;
+            } else {
+                return '';
+            }
+        });
+    } while (rAttr.lastIndex != 0)
+    console.log(content);
+
+    return content;
+}
