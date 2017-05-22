945ca1ac594f5d4d58e53658864956bf9c196da4
mengxiangxiang <mengxiangxiang@cdavatar.com>
2017-05-22 11:29:01 +0800

    新增 useCssModule 配置

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var cssModulesSync = require('postcss-modules-sync');
 var postcss = require('postcss');
 var htmlCompiler = require('vue-template-compiler');
+var _ = fis.util;
 
 module.exports = function (content, file, setting) {
+    var useCssModule = false;
+
+    if (_.has(setting, 'useCssModule')) {
+        useCssModule = setting.useCssModule;
+        delete setting.useCssModule;
+    }
+
     var output = htmlCompiler.parseComponent(content.toString(), { pad: true });
     var exportedTokens = {}, content;
     var rAttr = /(class[\s\n]*=[\s\n]*)('|")([^\2]*?)\2/g, result;
@@ -14,18 +22,22 @@ module.exports = function (content, file, setting) {
             var tokenFile, tokenContent;
 
             if (output['styles'].length == 1) {
-                styleFileName = file.realpathNoExt + '.css';
+                styleFileName = file.realpathNoExt + '.' + (item.lang || 'css');
             } else {
-                styleFileName = file.realpathNoExt + '-' + index + '.css';
+                styleFileName = file.realpathNoExt + '-' + index + '.' + (item.lang || 'css');
             }
 
-            var styles = postcss([
-                cssModulesSync.default({
-                    getTokens: function (tokens) {
-                        exportedTokens = fis.util.merge(exportedTokens ,tokens);
-                    }
-                })
-            ]).process(item.content).css;
+            if (useCssModule) {
+                var styles = postcss([
+                    cssModulesSync.default({
+                        getTokens: function (tokens) {
+                            exportedTokens = fis.util.merge(exportedTokens ,tokens);
+                        }
+                    })
+                ]).process(item.content).css;
+            } else {
+                var styles = item.content;
+            }
 
             // css
             styleFile = fis.file.wrap(styleFileName);
@@ -66,18 +78,20 @@ module.exports = function (content, file, setting) {
         output.template.content = '';
     }
     content = output.template.content;
-    do {
-        content = content.replace(rAttr, function (s, attrName, quote, attrValue) {
-            if (s && attrValue) {
-                // 'class=' + '"' + 'btn btn-info' + '"'
-                return attrName + quote + attrValue.replace(rClassName, function (s, className, index) {
-                    return (index ? ' ' : '') + (exportedTokens[className] || className);
-                }) + quote;
-            } else {
-                return '';
-            }
-        });
-    } while (rAttr.lastIndex != 0)
+    if (useCssModule) {
+        do {
+            content = content.replace(rAttr, function (s, attrName, quote, attrValue) {
+                if (s && attrValue) {
+                    // 'class=' + '"' + 'btn btn-info' + '"'
+                    return attrName + quote + attrValue.replace(rClassName, function (s, className, index) {
+                        return (index ? ' ' : '') + (exportedTokens[className] || className);
+                    }) + quote;
+                } else {
+                    return '';
+                }
+            });
+        } while (rAttr.lastIndex != 0)
+    }
 
     return content;
 }
